35c552ffdeb09a1701bf9884f917a0ac8f7fa534
Lucas Finoti Rodrigues <lucas-finoti@lucas.rodrigues>
2024-07-08 13:07:54 -0300

    refactor: configuration at database altered

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost', 
-      port: 3306, 
-      username: 'root',
-      password: 'root', 
-      database: 'nestjs', 
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true
-    }),
-    UserModule, 
+    DatabaseModule,
+    UserModule,
   ],
 })
 export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,33 @@
+import { Module, OnModuleInit, Logger } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+
+export class DatabaseModule implements OnModuleInit {
+  private readonly logger = new Logger(DatabaseModule.name);
+
+  onModuleInit() {
+    this.logger.log('Banco de dados está rodando');
+  }
+}
